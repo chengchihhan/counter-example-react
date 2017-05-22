e0070a864bf40f08e800732d9eee7014e3add3d1
chengchihhan <haneric1986@gmail.com>
2017-05-22 13:45:24 +0800

    insert counter component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import Counter from './Counter';
+import counter from './reducer';
+import { createStore } from 'redux';;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+store.subscribe(render);
+
+
