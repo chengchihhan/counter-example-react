0a5f03be27a94558969e0e6d6abb9064ec0e321c
chengchihhan <haneric1986@gmail.com>
2017-05-22 15:06:13 +0800

    use provider

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import Counter from './Counter';
 import reducer from './reducers/index';
 import { createStore } from 'redux';;
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
-const render = () => {
-  ReactDOM.render(
-    <Counter value={store.getState()}
-      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-    />,
-    document.getElementById('root')
-  );
-};
-
-render();
-
-store.subscribe(render);
-
+ReactDOM.render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root')
+);
 
