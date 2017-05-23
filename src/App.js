32811196b36bed090fcf6f3e5c62749b1c0cc2b1
chengchihhan <haneric1986@gmail.com>
2017-05-23 17:05:42 +0800

    convert to container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from './Counter';
+import * as CounterActions from './actions';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const mapStateToProps = (state) => ({
+  counter: state.value
+});
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(CounterActions, dispatch)
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
