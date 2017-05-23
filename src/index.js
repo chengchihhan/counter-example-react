44467d08c6a84a7f9958449be3aea8db782c0132
chengchihhan <haneric1986@gmail.com>
2017-05-23 17:07:30 +0800

    import App container

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import Counter from './Counter';
-import reducer from './reducers/index';
+import rootReducer from './reducers';
+import App from './App';
 
-
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Counter />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
